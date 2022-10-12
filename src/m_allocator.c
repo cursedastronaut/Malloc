57d9d9a0124882448573a11f8619d939f27f2788
g.martineaux <g.martineaux@student.isartdigital.com>
2022-10-12 12:19:01 +0000

    Added Calloc and Realloc

--- src/m_allocator.c
+++ src/m_allocator.c
@@ -16,12 +16,29 @@ void m_setup_hooks(void)
    __realloc_hook = realloc_hook;
    __free_hook = free_hook;
 }
- 
+#define COLOR_RED     "\x1b[31m"
+#define COLOR_GREEN   "\x1b[32m"
+#define COLOR_YELLOW  "\x1b[33m"
+#define COLOR_BLUE    "\x1b[34m"
+#define COLOR_MAGENTA "\x1b[35m"
+#define COLOR_CYAN    "\x1b[36m"
+#define COLOR_RESET   "\x1b[0m" 
 void m_show_info(void)
 {
+    printf(COLOR_YELLOW "Show the blocks (m_show_info):\n");
     for (Metadata* m = metadata; m != NULL; m = m->next)
     {
-        printf("Address: %p, Pointer: %p, Size: %ld, Free : %d , Next: %p\n", m, m->ptr, m->size, m->free, m->next);
+        printf(COLOR_GREEN "Adress : %p", m);
+        printf(COLOR_RESET " | ");
+        printf(COLOR_MAGENTA "Pointer : %p", m->ptr);
+        printf(COLOR_RESET " | ");
+        printf(COLOR_BLUE "Size : %4.ld", m->size);
+        printf(COLOR_RESET " | ");
+        printf(COLOR_YELLOW "Free : %d", m->free);
+        printf(COLOR_RESET " | ");
+        printf(COLOR_CYAN "Next : %14.p\n", m->next);
+        printf(COLOR_RESET "");
+        //printf(COLOR_RED "Address: %p, Pointer: %p, Size: %ld, Free : %d , Next: %p\n", m, m->ptr, m->size, m->free, m->next);
     }
 }
 
@@ -44,6 +61,28 @@ void fusion_block(Metadata* m1, Metadata* m2)
     m1->next = m2->next;
 }
 
+int auto_fusion_block(Metadata* prev)
+{
+    Metadata* block = prev->next;
+    if (prev->free) {
+        prev->size += block->size + sizeof(Metadata);
+        prev->next = block->next;
+        if (block->next != NULL)
+            if (block->next->free) {
+                prev->size += block->next->size + sizeof(Metadata);
+                prev->next = block->next->next;
+                return 2;
+            }
+        return -1;
+    } else if (block->next != NULL)
+        if (block->next->free) {
+            block->size += block->next->size + sizeof(Metadata);
+            block->next = block->next->next;
+            return 1;
+        }
+    return 0;
+}
+
 Metadata* get_free_block(size_t size)
 {
     for (Metadata* m = metadata; m != NULL; m = m->next)
@@ -57,6 +96,13 @@ Metadata* get_free_block(size_t size)
             
     return NULL; 
     
+}
+void* copy_value(void* newLocation, void* oldLocation)
+{
+    int* newVal = newLocation;
+    int* oldVal = oldLocation;
+    *newVal = *oldVal;
+    return newVal;
 }
 
 
@@ -99,13 +145,75 @@ void* m_malloc(size_t size)
  
 void* m_realloc(void* ptr, size_t size)
 {
+    if (size == 0) {
+        m_free(ptr);
+        return NULL;
+    }
+
+    if (ptr == NULL)
+        return m_malloc(size);
+
+    Metadata* data = NULL;
+    for (Metadata* m = metadata; m != NULL; m = m->next)
+        if (m->next != NULL)
+            if (m->next->ptr == ptr) {
+                data = m;
+                break;
+            }
+    if (data == NULL)
+        return NULL;
+
+    switch (auto_fusion_block(data)) {
+        case 1:
+            if (data->next->size == size)
+                return data->ptr;
+            if (data->next->size > size) {
+                if (split_block(data->next, size) == 0)
+                    return copy_value(m_malloc(size), ptr);
+                copy_value(data->ptr, ptr);
+                if (data->next->next == NULL)
+                    m_free(data->next->ptr);
+                return data->ptr;
+            }
+            data->next->free = true;
+            return copy_value(m_malloc(size), ptr);
+            
+        case 2:
+            if (data->size == size)
+                return data->ptr;
+            if (data->size > size) {
+                if (split_block(data, size) == 0)
+                    return copy_value(m_malloc(size), ptr);
+                copy_value(data->ptr, ptr);
+                if (data->next->next == NULL)
+                    m_free(data->next->ptr);
+                return data->ptr;
+            }
+            data->free = true;
+            return copy_value(m_malloc(size), ptr);
+
+        case 0:
+            data->next->free = true;
+            return copy_value(m_malloc(size), ptr);
+    }
+
     return NULL;
 }
  
 void* m_calloc(size_t nb, size_t size)
 {
-    
-    return m_malloc(size);
+    char *ptr;
+    size_t dat;
+
+    if ((ptr = m_malloc(size * nb)) == NULL)
+        return (NULL);
+    dat = 0;
+    while (dat < size)
+    {
+        ptr[dat] = 0;
+        ++dat;
+    }
+    return (ptr);
 }
  
 void m_free(void* ptr)
--- tests/test_allocator.c
+++ tests/test_allocator.c
@@ -1,15 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
 #include <m_allocator.h>
 
 void test_alloc()
 {
-    // MALLOC
-    /*int* a = malloc(sizeof(int));
-     *a = 10;*/
-    char *array1 = m_malloc(128 * sizeof(char));
+
+    /*char *array1 = m_malloc(128 * sizeof(char));
     strcpy(array1, "Bonjour");
 
     char *array2 = m_malloc(64 * sizeof(char));
@@ -17,11 +14,17 @@ void test_alloc()
     
     char *array3 = m_malloc(16 * sizeof(char));
     strcpy(array3, "Annyeong");
-    m_free(array2);
-    m_free(array1);
-
-    /*printf("(malloc) a3 (%p) = %s\n", array3, array3);*/
-    /*printf("(malloc) a4 (%p) = %s\n", array4, array4);*/
+    //m_free(array2);
+    //m_free(array1);
+    char* test2 = m_calloc(12, sizeof(int));
+    strcpy(test2, "AAAA");
+    printf("%s\n", test2);
+    m_realloc(test2, 999 * sizeof(int));
+    printf("%s\n", test2);
+    //printf("(malloc) a3 (%p) = %s\n", array3, array3);
+    //printf("(malloc) a4 (%p) = %s\n", array4, array4);*/
+    char *array1 = m_malloc(128 * sizeof(char));
+    strcpy(array1, "Bonjour");
     m_show_info();
 
     
